b998e0153b1b73ed98598d6d337701ca23bd0adf
Charlesdigital <charles.easton123@gmail.com>
2021-09-20 18:25:11 -0400

    Test refactor assertEqual and head

---
+++ test/assertEqualTest.js
@@ -0,0 +1,12 @@
+const assertEqual = require('../assertEqual');
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1,2);
+assertEqual(1,"1");
+
+// lotide/assertEqual.js
+//lotide/test/assertEqualTest.js
+//../../ 2 level
