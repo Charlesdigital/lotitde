16b2826d4708fd7f22f4d1156561157fc94eb9aa
Charlesdigital <charles.easton123@gmail.com>
2021-09-21 15:50:53 -0400

    incorporate Mocha and chai

--- middle.js
+++ middle.js
@@ -1,20 +1,26 @@
-function eqArrays(array1, array2) {
-    if(array1.length !== array2.length) {
-        return false
-    }
-    for(let i = 0; i < array1.length; i++) {
-      if(array1[i] !== array2[i]) {
-          return false
-      }
-    }
-    return true;
-}
 
-    const assertArraysEqual  = function(actual, expected) {
-        if (eqArrays(actual, expected)) {
-              console.log( `✅✅✅ Assertion Passed ${actual} === ${expected}`);
-          } else {console.log(`🛑🛑🛑 Assertion failed ${actual} !== ${expected}`)};
-      };
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
+
+
+
+// function eqArrays(array1, array2) {
+//     if(array1.length !== array2.length) {
+//         return false
+//     }
+//     for(let i = 0; i < array1.length; i++) {
+//       if(array1[i] !== array2[i]) {
+//           return false
+//       }
+//     }
+//     return true;
+// }
+
+//     const assertArraysEqual  = function(actual, expected) {
+//         if (eqArrays(actual, expected)) {
+//               console.log( `✅✅✅ Assertion Passed ${actual} === ${expected}`);
+//           } else {console.log(`🛑🛑🛑 Assertion failed ${actual} !== ${expected}`)};
+//       };
 
 const middle = function(array) {
 let middleArray = [];
@@ -32,6 +38,8 @@ if (array.length % 2 === 0) {
 }
 return middleArray
 }
+
+module.exports = middle;
 
 
 // console.log(middle([1]))
